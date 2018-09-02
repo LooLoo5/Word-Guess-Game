7045d8d48abd247b453fa005aeeda16a711a5cca
Emily Lorraine <emilylorraine@Emilys-MacBook-Air.local>
2018-09-02 14:22:57 -0500

    Second variable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,27 @@
 prompt("Press any key to start the game!");
 
-
+//Game variables
     var alphabetArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "x", "z"];
 
     var letter = alphabetArr[Math.floor(Math.random() * alphabetArr.length)];
 
-    var incorrectLetter = [0];
+    var playerGuess = 0;
 
-    var correctLetter = [0];
+    var ranLetter = ranLetter;
 
-    var remainingGuesses = 15;
+prompt("Guess What Letter I am Thinking of!");
 
-    document.onkeyup = function(event){
-        var userGuess= event.key;
+element.onkeyup = function (event) {
+    var playerGuess = keyWasPressed;
+}
+
+
+    if (playerGuess === ranLetter) {
+        correct++;
+        playerGuess = [];
     }
 
-    for (var z = 0; z < alphabetArr; z++) {
-        if (userGuess === letter);
-        alert("Good Guess!")
-    
-    }
+  var newGuess= alphabetArr[Math.floor(Math.random()*26)];
+    ranLetter=newGuess;
+    console.log("new guess is :" + newGuess);
 
