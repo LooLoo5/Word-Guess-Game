ff0520f38ec87ba43b30088b0f5cc4bdd3340ea3
Emily Lorraine <emilylorraine@Emilys-MacBook-Air.local>
2018-09-08 13:48:00 -0500

    console success

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,40 @@
 
 
 //Game variables
-    var alphabetArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "x", "z"];
+var alphabetArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "y", "x", "z"];
 
-    var letter = alphabetArr[Math.floor(Math.random() * alphabetArr.length)];
+var letter = alphabetArr[Math.floor(Math.random() * alphabetArr.length)];
 
-    var playerGuess = [];
+var playerGuess = [];
 
-    var ranLetter = ranLetter;
+var computerChoice = letter;
 
-    console.log(letter);
- //started to try a new "begining" comand
-function startGame(){
+console.log(letter);
+//started to try a new "begining" comand
+function startGame() {
     playerGuess[letter] = letter;
     document.getElementById("displayRandomLetter").innerHTML = letter;
+    console.log(computerChoice);
 
+    document.onkeyup = function (event) {
+        event.key = computerChoice
+        console.log("Bring on the letters")
 
- var computerChoice = alphabetArr[letter];
+        if (playerGuess === computerChoice) {
+            correct++;
+            playerGuess = [];
+        } else {
+            (playerGuess !== computerChoice)
+        }
 
+        var newGuess = alphabetArr[Math.floor(Math.random() * 26)];
+        computerChoice = newGuess;
+        console.log("new guess is :" + newGuess);
 
-function.onkeyup = function(event) {
-    var playerGuess = event.key;
-}
-
-
-    if (playerGuess === ranLetter) {
-        correct++;
-        playerGuess = [];
     }
+};
 
-  var newGuess= alphabetArr[Math.floor(Math.random()*26)];
-    ranLetter=newGuess;
-    console.log("new guess is :" + newGuess);
-}
+
+
 startGame();
+
